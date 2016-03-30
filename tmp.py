4a9c820680bbf0ef0703b97bbbd5bf7001ea6845
viktor.kits@basetis.com <viktor.kits@basetis.com>
2016-03-30 17:25:46 +0200

    tmp

---
+++ tmp.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Tmp(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://127.0.0.1:8069/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_tmp(self):
+        driver = self.driver
+        driver.get(self.base_url + "/web#id=3444&view_type=form&model=hr_timesheet_sheet.sheet&menu_id=489")
+        driver.find_element_by_xpath("//div[3]/button").click()
+        driver.find_element_by_link_text("Suprimir").click()
+        self.assertRegexpMatches(self.close_alert_and_get_its_text(), r"^¿Realmente desea eliminar este registro[\s\S]$")
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
