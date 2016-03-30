ff8672bdc60d3ced19ca2b833cc8fae0d9d014ee
viktor.kits@basetis.com <viktor.kits@basetis.com>
2016-03-30 17:32:56 +0200

    add test_horas

--- run.py
+++ run.py
@@ -5,12 +5,14 @@ import unittest
 from Odoo_testing_0_Login import Odoo_testing_0_Login
 from Odoo_testing_1_Menu import Odoo_testing_1_Menu
 from Odoo_testing_2_Personas import Odoo_testing_2_Personas
+from Odoo_testing_3_horas import Odoo_testing_3_horas
 
 odoo_test_0 = unittest.TestLoader().loadTestsFromTestCase(Odoo_testing_0_Login)
 odoo_test_1 = unittest.TestLoader().loadTestsFromTestCase(Odoo_testing_1_Menu)
 odoo_test_2 = unittest.TestLoader().loadTestsFromTestCase(Odoo_testing_2_Personas)
+odoo_test_3 = unittest.TestLoader().loadTestsFromTestCase(Odoo_testing_3_horas)
 
-final_tests = unittest.TestSuite([odoo_test_0, odoo_test_1, odoo_test_2])
+final_tests = unittest.TestSuite([odoo_test_0, odoo_test_1, odoo_test_2, odoo_test_3])
 
 #unittest.TextTestRunner().run(final_tests)
 
